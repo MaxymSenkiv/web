337498fc26db24a18ed0d5f7df13f65f7895a3f0
MaxymSenkiv <maksym.senkiv.knm.2019@lpnu.ua>
2021-05-28 06:52:18 +0300

    lab 3,4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import RenderLogin from "./static/js/login";
+import RenderSignup from "./static/js/signup";
+import RenderMenu from "./static/js/menu";
+import RenderUser from "./static/js/user";
+import {Component} from "react";
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+        <Router>
+            <div className={"Login"}>
+                <Switch>
+                    <Route path='/login' exact component={RenderLogin}/>
+                    <Route path='/signup'  component={RenderSignup}/>
+                    <Route path='/menu'  component={RenderMenu}/>
+                    <Route path='/user'  component={RenderUser}/>
+                </Switch>
+            </div>
+        </Router>
+    );
+  }
 }
 
 export default App;
